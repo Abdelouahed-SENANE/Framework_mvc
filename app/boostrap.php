a2b02c21dc35b3bb2811df18e2b1e9fa93d04d4f
AbdelouhedSN <abdosenane75@gmail.com>
2023-12-22 20:09:37 +0100

    Add new Update

--- app/boostrap.php
+++ app/boostrap.php
@@ -1,19 +1,26 @@
-<?php 
 
+<?php
 require_once 'config/config.php';
 
 
+function Autoloader($class) {
+    $paths = [
+        APPROOT."/libraries/",
+        APPROOT."/services/",
+        APPROOT."/models/",
+        APPROOT."/security/"
+    ];
 
-// Autoload Libraries 
+    foreach ($paths as $path) {
+        $file = $path . $class . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+}
 
 
-
-spl_autoload_register(function($className){
-
-    require_once 'libraries/' . $className . '.php';
-
-});
+spl_autoload_register('Autoloader');
 
 $init = new Core();
-
 ?>
