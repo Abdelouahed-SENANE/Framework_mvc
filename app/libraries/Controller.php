8b00548f8f2cc04afee097f36a5bf404c70a5dac
AbdelouhedSN <abdosenane75@gmail.com>
2024-01-08 09:55:12 +0100

    Add function to required in Controllers

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -8,6 +8,19 @@
             // Instance model
             return new $model();
         }
+        // load Service  ================
+        public function service($service) {
+            $servicePathFile = '../app/services/'. $service .'.php';
+            // Require Service file 
+            if (file_exists($servicePathFile)) {
+                require_once $servicePathFile;
+                // Instance Service
+                return new $service();
+            }else {
+                echo "File not Exists";
+            }
+            
+        }
         // load View 
         public function view($view , $data = []) {
             // Check for View file 
